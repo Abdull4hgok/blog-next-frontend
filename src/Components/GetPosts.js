2c2f0e60f7d6d779c719119711c2d0c5f2d792f1
Abdull4hgok <agok106@gmail.com>
2023-07-13 13:18:37 +0300

    Getpost fixed

--- src/Components/GetPosts.js
+++ src/Components/GetPosts.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { useRouter } from 'next/router';
 import { axiosapi } from './Auth';
 import Swal from 'sweetalert2';
-import './posts.css'
+import './posts.css';
 import logo from '../app/images/Logo.png';
 import kod from '../app/images/kod.jpg';
 
@@ -32,33 +32,34 @@ function GetPosts() {
   const [currentPage, setCurrentPage] = useState(1); // Şu anki sayfa numarası
   const perPage = 10; // Her sayfada gösterilecek gönderi sayısı
 
-useEffect(() => {
-  const fetchInitialPosts = async () => {
-    await fetchPosts();
-
-    const storedCategory = sessionStorage.getItem('selectedCategory'); // sessionStorage'dan seçili kategoriyi al
-    if (storedCategory) {
-      setSelectedCategory(storedCategory); // Seçili kategoriyi güncelle
-    }
-  };
-
-  fetchInitialPosts();
-
-  fetchCategories();
-}, [currentPage,selectedCategory]);
-
+    // Use Effects
 
   useEffect(() => {
-    
+    const fetchInitialPosts = async () => {
+      await fetchPosts();
+
+      const storedCategory = sessionStorage.getItem('selectedCategory'); // sessionStorage'dan seçili kategoriyi al
+      if (storedCategory) {
+        setSelectedCategory(storedCategory); // Seçili kategoriyi güncelle
+      }
+    };
+
+    fetchInitialPosts();
+
+    fetchCategories();
+  }, [currentPage, selectedCategory]);
+
+  useEffect(() => {
     filterPosts();
-    
   }, [allPosts, selectedCategory]);
-  
+
   useEffect(() => {
     if (router.query.category) {
       setSelectedCategory(router.query.category);
     }
   }, [router.query]);
+
+   // Get Posts
 
   const fetchPosts = async () => {
     try {
@@ -75,16 +76,18 @@ useEffect(() => {
       setErrorMessage('Yetkisiz erişim. Lütfen giriş yapın.');
     }
   };
-  
- 
+
+  //Categories
+
   const filterPosts = () => {
     if (selectedCategory === '') {
       setPosts(allPosts);
     } else {
-      const filtered = allPosts.filter(post => post.category === selectedCategory);
+      const filtered = allPosts.filter((post) => post.category === selectedCategory);
       setPosts(filtered);
     }
   };
+
   const fetchCategories = async () => {
     try {
       const response = await axiosapi.get('http://127.0.0.1:8000/api/get-categories');
@@ -95,15 +98,50 @@ useEffect(() => {
     }
   };
 
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
+  const handleCategoryFilter = async (category) => {
+    setSelectedCategory(category);
 
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
+    try {
+      const response = await axiosapi.get('http://127.0.0.1:8000/api/posts', {
+        params: { category, page: 1, perPage },
+      });
+      console.log(response.data.data);
+      setFilteredPosts(response.data.data);
+      const filteredUrl = `posts/?id=category=${category}`;
+      router.push(filteredUrl);
+    } catch (error) {
+      console.error('Gönderiler getirilirken hata oluştu:', error);
     }
   };
+
+  const handleClearCategoryFilter = async () => {
+    setSelectedCategory(''); // Seçili kategoriyi temizle
+    sessionStorage.removeItem('selectedCategory'); // sessionStorage'dan seçili kategoriyi sil
+
+    try {
+      const response = await axiosapi.get('http://127.0.0.1:8000/api/posts', {
+        params: { page: 1, perPage }, // category parametresini boş olarak gönder
+      });
+      console.log(response.data.data);
+      setPosts(response.data.data);
+      setFilteredPosts(response.data.data);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewPostData((prevData) => ({
+      ...prevData,
+      [name]: name === 'category' ? parseInt(value) : value,
+    }));
+    if (name === 'category') {
+      setSelectedCategory(value); // Bu satırı ekleyin
+    }
+  };
+
+  // Delete Post
 
   const handleDelete = async (postId) => {
     try {
@@ -115,11 +153,11 @@ useEffect(() => {
         confirmButtonText: 'Evet, sil!',
         cancelButtonText: 'İptal',
       });
-  
+
       if (confirmed.isConfirmed) {
         await axiosapi.get(`http://127.0.0.1:8000/api/delete/${postId}`);
         console.log('Post deleted');
-  
+
         setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
         setFilteredPosts((prevFilteredPosts) =>
           prevFilteredPosts.filter((post) => post.id !== postId)
@@ -134,32 +172,13 @@ useEffect(() => {
       console.error('Error deleting post:', error);
     }
   };
-  
 
   
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-    setNewPostData({
-      title: '',
-      content: '',
-      description: '',
-      category: '',
-    });
-    setErrorMessage('');
-  };
+ 
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setNewPostData((prevData) => ({
-      ...prevData,
-      [name]: name === 'category' ? parseInt(value) : value,
-    }));
-    if (name === 'category') {
-      setSelectedCategory(value); // Bu satırı ekleyin
-    }
-  };
 
+  // Add Post
   const handleAddPost = async (event) => {
     event.preventDefault();
 
@@ -190,72 +209,58 @@ useEffect(() => {
     }
   };
 
-const handleSearch = async (event, page) => {
-  const searchValue = event.target.value;
-  setSearchTerm(searchValue);
-
-  try {
-    const response = await axiosapi.get('http://127.0.0.1:8000/api/search', {
-      params: {
-        term: searchValue,
-        page: page, // Sayfa numarasını gönder
-        perPage: 10 // Sayfa boyutunu belirle (örneğin 10 olarak kabul edelim)
-      },
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setNewPostData({
+      title: '',
+      content: '',
+      description: '',
+      category: '',
     });
-    const filtered = response.data.data; // Sayfalanmış verileri al
-    setFilteredPosts(filtered);
-    const searchUrl = `posts/?search=${encodeURIComponent(searchValue)}`;
-    router.push(searchUrl);
-  } catch (error) {
-    console.error('Error searching posts:', error);
-  }
-};
+    setErrorMessage('');
+  };
 
-  const handleCategoryFilter = async (category) => {
-    setSelectedCategory(category);
-  
+  // Search Post
+
+  const handleSearch = async (event, page) => {
+    const searchValue = event.target.value;
+    setSearchTerm(searchValue);
+
     try {
-      const response = await axiosapi.get('http://127.0.0.1:8000/api/posts', {
-        params: { category, page: 1, perPage },
+      const response = await axiosapi.get('http://127.0.0.1:8000/api/search', {
+        params: {
+          term: searchValue,
+          page: page, // Sayfa numarasını gönder
+          perPage: 10, // Sayfa boyutunu belirle (örneğin 10 olarak kabul edelim)
+        },
       });
-      console.log(response.data.data);
-      setFilteredPosts(response.data.data);
-      const filteredUrl = `posts/?id=category=${category}`;
-      router.push(filteredUrl);
-
+      const filtered = response.data.data; // Sayfalanmış verileri al
+      setFilteredPosts(filtered);
+      const searchUrl = `posts/?search=${encodeURIComponent(searchValue)}`;
+      router.push(searchUrl);
     } catch (error) {
-      console.error('Gönderiler getirilirken hata oluştu:', error);
+      console.error('Error searching posts:', error);
     }
   };
-  
-  
-  
 
-  const handleClearCategoryFilter = async () => {
-    setSelectedCategory(''); // Seçili kategoriyi temizle
-    sessionStorage.removeItem('selectedCategory'); // sessionStorage'dan seçili kategoriyi sil
-  
-    try {
-      const response = await axiosapi.get('http://127.0.0.1:8000/api/posts', {
-        params: { page: 1, perPage }, // category parametresini boş olarak gönder
-      });
-      console.log(response.data.data);
-      setPosts(response.data.data);
-      setFilteredPosts(response.data.data);
-    } catch (error) {
-      console.error('Error fetching posts:', error);
+  // Pagination
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
     }
   };
-  
 
-  
 
   return (
     <div>
-      <div className="container" >
-      
-      <div className="col-md-5 search">
-            <div className="input-group">
+      <div className="container">
+        <div className="col-md-5 search">
+          <div className="input-group">
             <input
               type="text"
               className="form-control"
@@ -263,110 +268,109 @@ const handleSearch = async (event, page) => {
               value={searchTerm}
               onChange={(event) => handleSearch(event, currentPage)} // handleSearch fonksiyonunu çağırın
             />
-
-            </div>
-        
-          </div>
-      
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <Image src={logo} width={50} height={50} />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <div>
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-
-                </div>
-              </li>
-          
-              
-            </ul>
           </div>
         </div>
-      </nav>
-     
-      <div className="header">
-  <h2>POSTS</h2>
-</div>
-<button className="btn btn-primary add-post" onClick={() => setIsModalOpen(true)}>
-    Add Post
-      </button>
-<div className='row'>
 
-      
-      </div>
-<div className="row">
-  <div className="leftcolumn">
-  {filteredPosts.map((post, index) => (
-    <div className="card"  key={post.id}>
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-      <div className='row action'>
-      <Link className="col-md-2" href={`/detail?id=${post.id}`}>
-              <span className="btn nav-link dsc no-underline">Devamını Oku...</span>
-            </Link>
-    <button
-      className="btn btn-danger col-md-1 sil"
-      onClick={() => handleDelete(post.id)}
-    >
-      Sil
-    </button>
-    </div>
-    </div>
-   ))}
-  </div>
-  <div className="rightcolumn">
-    <div className="card">
-      <h2>What is Lorem Ipsum?</h2>
-      <Image src={kod} width={245} height={100} />
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <Image src={logo} width={50} height={50} />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <div>
+                    <a className="nav-link active" aria-current="page" href="/">
+                      Home
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-      <p>It is a long established fact that a reader will be distracted by the readable content of a page when</p>
-    </div>
-    <div className="card">
-      <h3 className='text-center'>Categories</h3>
-      <div className="categories">
-    <button
-      className={`btn nav-link ${selectedCategory === '' ? 'active' : ''}`}
-      onClick={handleClearCategoryFilter} // handleClearCategoryFilter fonksiyonunu çağırın
-    >
-      Tüm Kategoriler
-    </button>
-    {categories.map((category) => (
-      <button
-        key={category.id}
-        className={`btn nav-link ${
-          selectedCategory === category.id ? 'active' : ''
-        }`}
-        onClick={() => handleCategoryFilter(category.id)} // handleCategoryFilter fonksiyonunu çağırın
-      >
-        {category.name}
-      </button>
-    ))}
-  </div>
-    </div>
-    <div className="card">
-      <h3>What is Lorem Ipsum?</h3>
-      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</p>
-    </div>
-  </div>
-</div>
+        <div className="header">
+          <h2>POSTS</h2>
+        </div>
+        <button className="btn btn-primary add-post" onClick={() => setIsModalOpen(true)}>
+          Add Post
+        </button>
+        <div className="row"></div>
+        <div className="row">
+          <div className="leftcolumn">
+            {filteredPosts.map((post, index) => (
+              <div className="card" key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+                <div className="row action">
+                  <Link className="col-md-2" href={`/detail?id=${post.id}`}>
+                    <span className="btn nav-link dsc no-underline">Devamını Oku...</span>
+                  </Link>
+                  <button
+                    className="btn btn-danger col-md-1 sil"
+                    onClick={() => handleDelete(post.id)}
+                  >
+                    Sil
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="rightcolumn">
+            <div className="card">
+              <h2>What is Lorem Ipsum?</h2>
+              <Image src={kod} width={245} height={100} />
+              <p>
+                It is a long established fact that a reader will be distracted by the readable
+                content of a page when
+              </p>
+            </div>
+            <div className="card">
+              <h3 className="text-center">Categories</h3>
+              <div className="categories">
+                <button
+                  className={`btn nav-link ${selectedCategory === '' ? 'active' : ''}`}
+                  onClick={handleClearCategoryFilter} // handleClearCategoryFilter fonksiyonunu çağırın
+                >
+                  Tüm Kategoriler
+                </button>
+                {categories.map((category) => (
+                  <button
+                    key={category.id}
+                    className={`btn nav-link ${
+                      selectedCategory === category.id ? 'active' : ''
+                    }`}
+                    onClick={() => handleCategoryFilter(category.id)} // handleCategoryFilter fonksiyonunu çağırın
+                  >
+                    {category.name}
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="card">
+              <h3>What is Lorem Ipsum?</h3>
+              <p>
+                It is a long established fact that a reader will be distracted by the readable
+                content of a page when looking at its layout. The point of using Lorem Ipsum
+              </p>
+            </div>
+          </div>
+        </div>
 
-<div className="footer">
-  <h2>
+        <div className="footer">
+          <h2>
             <div className="text-center page">
               <button
                 className="btn btn-primary me-2"
@@ -382,8 +386,9 @@ const handleSearch = async (event, page) => {
               >
                 Sonraki Sayfa
               </button>
-            </div></h2>
-</div>
+            </div>
+          </h2>
+        </div>
         {isModalOpen && (
           <div className="modal show" tabIndex="-1" style={{ display: 'block' }}>
             <div className="modal-dialog">
